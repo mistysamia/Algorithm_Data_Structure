28ea73b0c32ad5048df8f29c204fc2a537c199d0
Misty Samia <62679682+mistysamia@users.noreply.github.com>
2022-11-04 04:08:35 +0600

    Update Square Root.cpp

--- Binary Search/Square Root.cpp
+++ Binary Search/Square Root.cpp
@@ -1,4 +1,35 @@
-///Will return the square root
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define pb push_back
+#define MP make_pair
+#define llu unsigned long long
+#define U unsigned int
+#define ff first
+#define ss second
+#define all(v) (v).begin(), (v).end()
+#define b(n) cout << "bug " << n << endl;
+#define SIZE 100005
+#define pie 3.14159265358979323
+#define minuss 1e-6
+#define pii pair<ll, ll>
+#define fast_cin                                                               \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(NULL)
+#define BIG 1000000000
+#define INF 1000000000000005
+#define N 10000
+
+ll mod = 1e9 + 7;
+
+// GCD=__gcd(n,m);  lcm=(n*m)/GCD;
+// cout<<fixed<<setprecision(1)<<VALUE NAME<<endl;
+
+ll tra[100006], tra1[100006];
+
+////searching from array the last occurrence, return index if found, return -1 if not found
+
 double squareRoot(double n) {
     double low = 0, high = 1e4;
     for(int i=0; i<100; i++) {
@@ -10,6 +41,34 @@ double squareRoot(double n) {
             low = mid;
         }
     }
-
     return low;
 }
+
+int main() {
+  fast_cin;
+  ll t, cas = 1;
+  cin >> t;
+  while (t--) {
+    map<ll, ll> mp;
+    //  map<ll,ll>::iterator it;
+    string s, s1;
+    deque<ll> deq;
+    ll lock = 0, sum = 0, co = 0, a = 0, b = 0, c = 0, d = 0;
+    ll n, m, k, mx = -BIG, mn = BIG;
+    ll fir = 0, sec = 0, x = 0, y = 0, point, in, temp;
+    double key,ans;
+ 
+    cin >> key;
+    ans = squareRoot(key);
+
+    cout << "Output" << endl;
+    cout << ans << endl;
+    
+    // ll str[n+3];
+    // ll len=s.length();
+  }
+}
+
+/*
+
+*/
