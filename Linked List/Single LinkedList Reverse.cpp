6cae83c380f27b8c7a6ddc59b4e41a5416cae697
MISTY\misty <misty.samia17@gmail.com>
2024-03-21 07:09:46 +0600

    Update LinkedList Single Reverse Code

--- Linked List/Single LinkedList Reverse.cpp
+++ Linked List/Single LinkedList Reverse.cpp
@@ -1,23 +1,56 @@
 #define ll long long
-struct node {
+
+// Approach 1 : 
+struct node
+{
   ll data;
   struct node *next;
 };
 typedef struct node newNode;
 newNode *head = NULL, *tail = NULL;
 
-void reverse(newNode *&head, newNode *&tail) {
+void reverse(newNode *&head, newNode *&tail)
+{
   newNode *current = head, *temp = NULL, *newNext = NULL;
-  tail=head;
-  while (current != NULL) {
+  tail = head;
+  while (current != NULL)
+  {
     temp = current->next;
     current->next = newNext;
     newNext = current;
-    if(temp==NULL){
-        head=current;
-        return;
+    if (temp == NULL)
+    {
+      head = current;
+      return;
     }
     current = temp;
   }
-  cout<<"Nothing to reverse"<<endl;
+  cout << "Nothing to reverse" << endl;
+}
+
+
+// Approach 2 : 
+
+// Definition for singly-linked list.
+struct ListNode
+{
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+ListNode *reverseList(ListNode *head)
+{
+  ListNode *current = head, *prev = NULL;
+
+  while (current != NULL)
+  {
+    ListNode *temp = current->next;
+    current->next = prev;
+    prev = current;
+    current = temp;
+  }
+  return prev;
 }
